65f4814ece3b935ba61bffcc11640667b39b62cd
Jor_Poon <jormpoon@gmail.com>
2019-04-10 18:28:16 -0400

    Dashboard testing rendering of components Dashboard, Displays and Controls

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -6,3 +6,31 @@ import renderer from 'react-test-renderer';
 import 'jest-dom/extend-expect';
 import 'react-testing-library/cleanup-after-each';
 
+import Dashboard from './Dashboard';
+import Display from '../display/Display'
+import Controls from '../controls/Controls'
+
+describe('<Dashboard />', () => {
+    it.skip('matches snapshot', () => {
+        const tree = renderer.create(<Dashboard/>).toJSON();
+        expect(tree).toMatchSnapShot();
+    })
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Dashboard />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    })
+
+    it('renders successfully', () => {
+        render(<Dashboard/>)
+    })
+
+    it('renders successfully', () => {
+        render(<Display/>)
+    })
+
+    it('renders successfully', () => {
+        render(<Controls/>)
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -5,3 +5,45 @@ import {render, fireEvent} from 'react-testing-library';
 import renderer from 'react-test-renderer';
 import 'jest-dom/extend-expect';
 import 'react-testing-library/cleanup-after-each';
+
+import Display from './Display'
+import Controls from '../controls/Controls'
+import Dashboard from '../dashboard/Dashboard'
+
+describe('<Display />', () => {
+    it.skip('matches snapshot', () => {
+        const tree = renderer.create(<Display/>).toJSON();
+        expect(tree).toMatchSnapShot();
+    })
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Display />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    })
+
+    it('renders successfully', () => {
+        render(<Display/>)
+    })
+
+    it(' default renders Open', () => {
+        const {getByText} = render(<Display/>)
+        const div = getByText(/Open/i)
+    })
+
+    it(' default renders Unlocked', () => {
+        const {getByText} = render(<Display/>)
+        const div = getByText(/unlocked/i)
+    })
+
+    it('renders Closed when closed button is click', () => {
+        const {getByText} = render(<Dashboard/>)
+        const div = getByText(/Open/i)
+        const button = getByText(/close gate/i)
+
+        fireEvent.click(button)
+        getByText(/Closed/i);
+    })
+
+
+})
