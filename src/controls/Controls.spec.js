2c6ffe5e6934aa7783b4b3f2c94a85478b16bc7e
Jor_Poon <jormpoon@gmail.com>
2019-04-10 17:17:58 -0400

    made 2 tests for rendering Control component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,26 @@
 // Test away!
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {render, fireEvent} from 'react-testing-library';
+import renderer from 'react-test-renderer';
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
+
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+    // it.skip('matches snapshot', () => {
+    //     const tree = renderer.create(<Controls/>).toJSON();
+    //     expect(tree).toMatchSnapShot();
+    // })
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Controls />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    })
+
+    it('renders successfully', () => {
+        render(<Controls/>)
+    })
+})
